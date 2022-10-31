bfb4374ff621d93916116f4ba3eced3271ab561b
hobbleabbas <shakeel@atlasfinance.org>
2022-10-30 23:49:06 -0400

    update link

--- pages/case-study/dewji-realty.js
+++ pages/case-study/dewji-realty.js
@@ -77,7 +77,7 @@ export default function Arezo() {
 
             <div className="my-20 sm:my-24" />
 
-            <a href="https://www.youtube.com/watch?v=6Zg0QZ1J3Z8" target="_blank">
+            <a href="https://www.youtube.com/watch?v=A3LaRXxoTwY" target="_blank">
                 <video
                     autoPlay
                     loop

4632cfa8b4af50f742bc0dc3a24b2a4c0a32a83c
hobbleabbas <shakeel@atlasfinance.org>
2022-11-12 19:18:29 -0500

    fgc

--- pages/case-study/friday-grooming-co.js
+++ pages/case-study/friday-grooming-co.js
@@ -2,6 +2,17 @@ import Footer from "../../components/Footer";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import Navbar from "../../components/Navbar";
 import NextPage from "../../components/Nextpage";
+import firstImage from "../../public/casestudies/fgc/1.jpg";
+import secondImage from "../../public/casestudies/fgc/2.jpg";
+import thirdImage from "../../public/casestudies/fgc/3.jpg";
+import fourthImage from "../../public/casestudies/fgc/4.jpg";
+import fifthImage from "../../public/casestudies/fgc/5.jpg";
+import sixthImage from "../../public/casestudies/fgc/6.jpg";
+import seventhImage from "../../public/casestudies/fgc/7.jpg";
+import eighthImage from "../../public/casestudies/fgc/8.jpg";
+import ninthImage from "../../public/casestudies/fgc/9.jpg";
+import tenthImage from "../../public/casestudies/fgc/10.jpg";
+import eleventhImage from "../../public/casestudies/fgc/11.jpg";
 
 export default function Arezo() {
 
@@ -34,8 +45,117 @@ export default function Arezo() {
 
             <div className="my-20 sm:my-24" />
 
-            <LoadingSpinner />
-            
+            <div style={{
+                backgroundImage: `url(${firstImage.src})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center"
+                
+                }}>
+                <div className="h-screen w-full">
+                </div>
+            </div>
+
+            <div className="p-4 sm:p-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
+                <div style={{
+                    backgroundImage: `url(${secondImage.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center"
+                    
+                    }}>
+                    <div className="h-108 w-full">
+                    </div>
+                </div>
+
+                <div style={{
+                    backgroundImage: `url(${thirdImage.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center"
+                    
+                    }} className="sm:col-span-2">
+                    <div className="h-108 w-full">
+                    </div>
+                </div>
+            </div>
+
+            <div style={{
+                backgroundImage: `url(${fourthImage.src})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center"
+                
+                }}>
+                <div className="h-screen w-full">
+                </div>
+            </div>
+
+            <div className = "my-4 sm:my-8" />
+
+            <div style={{
+                backgroundImage: `url(${fifthImage.src})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center"
+                
+                }}>
+                <div className="h-screen w-full">
+                </div>
+            </div>
+
+            <div className="p-4 sm:p-8 grid grid-cols-1 gap-8 sm:grid-cols-2">
+                <div style={{
+                    backgroundImage: `url(${sixthImage.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center"
+                    
+                    }}>
+                    <div className="h-108 w-full">
+                    </div>
+                </div>
+
+                <div style={{
+                    backgroundImage: `url(${seventhImage.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center"
+                    
+                    }}>
+                    <div className="h-108 w-full">
+                    </div>
+                </div>
+            </div>
+
+            <div className="px-36 sm:px-48 md:px-96">
+                <img src={eighthImage.src} />
+            </div>
+
+            <div className="p-4 sm:p-8 grid grid-cols-1 gap-8 sm:grid-cols-2">
+                <div style={{
+                    backgroundImage: `url(${ninthImage.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center"
+                    
+                    }}>
+                    <div className="h-108 w-full">
+                    </div>
+                </div>
+
+                <div style={{
+                    backgroundImage: `url(${tenthImage.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center"
+                    
+                    }}>
+                    <div className="h-108 w-full">
+                    </div>
+                </div>
+            </div>
+
             <NextPage
                 text={"Xana International"}
                 href={"/case-study/xana-international"}
